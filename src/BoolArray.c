b118008a862d26b0cad8ffa2969009f529ceb12b
fusiled <fusiled@gmail.com>
2016-09-29 10:58:04 +0200

    fix2

--- src/BoolArray.c
+++ src/BoolArray.c
@@ -119,7 +119,8 @@ int getSize(BoolArray * bArr)
 unsigned char isEmpty(BoolArray * bArr)
 {
 	char empty_flag = 1;
-	#pragma omp parallel for schedule(static)
+	#pragma omp parallel shared(empty_flag)
+	#pragma omp for schedule(static) nowait
 	for(int i=0; i < empty_flag*getSize(bArr); i=i+CHAR_SIZE)
 	{
 		if(get_chunk(bArr, i)!=0)
