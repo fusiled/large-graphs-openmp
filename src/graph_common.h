7836c17d0ac380e3c135ab4f2e7542c632e51d55
Matteo Fusi <fusiled@gmail.com>
2016-10-02 12:17:53 +0200

    fixed header files

--- src/graph_common.h
+++ src/graph_common.h
@@ -3,12 +3,15 @@
 #ifndef GRAPH_COMMON_H
 #define GRAPH_COMMON_H
 
+#include <limits.h>
 
 #define GRAPH_WITH_WEIGHT 1
 #define GRAPH_WITHOUT_WEIGHT 0
 
 #define GRAPH_TYPE_ADJ_LIST 0
 #define GRAPH_TYPE_MATRIX 1
+
+#define INF INT_MAX
 
 #ifdef TEST
 	#include <stdio.h>
