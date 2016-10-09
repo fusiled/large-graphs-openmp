9c50c6a3866ed970c1748030f9234aba432476d2
Matteo Fusi <fusiled@gmail.com>
2016-10-09 10:59:46 +0200

    fixed test files

--- test_linked.bash
+++ test_linked.bash
@@ -2,21 +2,26 @@
 
 #executables="main_bfs_run main_sssp_run main_apsp_sssp_run"
 executables="main_bfs_run"
-output_file="test_result/test_output_linked"
+output_file="test_result/test_output_linked_"`date -Iseconds`
 graph_path="graphs/test_graph.gr"
 
 graph_type="0"
 CORE_CAP="24"
 n_vert_pow="3"
 
-rm -f "$output_file"
+#make csv header
+for exec in $executables
+	do
+		printf "n_vert," >>"$output_file"_"$exec"
+		printf "n_threads," >>"$output_file"_"$exec"
+		printf "t\n">> "$output_file"_"$exec"
+done
 #generate a graph with increasing number of vertices
 while  [ $n_vert_pow -lt 8 ]
 do
 	n_vert=$(awk "BEGIN{print 10 ** $n_vert_pow}")
 	./Randgraph/rg $graph_path $n_vert $graph_type 11
 	echo "generated graph with $n_vert vertices"
-	echo "n_vert=$n_vert">>$output_file
 	#iterate over num_cores
 	core_power="0"
 	while [ $core_power -lt 6 ]
@@ -27,10 +32,12 @@ do
 			n_core="24"
 		fi
 		#exec all the algos
-		echo "n_thread=$n_core">>$output_file
 		for exec in $executables
 		do
-			"bin/$exec" "$n_core" "$graph_path" 1 0 >> "$output_file"
+			printf "%s," "$n_vert" >>"$output_file"_"$exec"
+			printf "%s," "$n_core"  >>"$output_file"_"$exec"
+			time_res=`bin/$exec $n_core $graph_path 1 0`
+			printf "%s\n"  $time_res >> "$output_file"_"$exec"
 		done
 		core_power=$[$core_power+1]
 	done
--- test_matrix.bash
+++ test_matrix.bash
@@ -1,39 +1,46 @@
 #!/bin/bash
 
 executables="main_apsp_fw_run"
-output_file="test_result/test_output_matrix"
+output_file="test_result/test_output_matrix_"`date -Iseconds`
 graph_path="graphs/test_graph.gr"
 
 graph_type="0"
 CORE_CAP="24"
 n_vert_pow="3"
 
-rm -f "$output_file"
+#make csv header
+for exec in $executables
+	do
+		printf "n_vert," >>"$output_file"_"$exec"
+		printf "n_threads," >>"$output_file"_"$exec"
+		printf "t\n">> "$output_file"_"$exec"
+done
 #generate a graph with increasing number of vertices
-while  [ $n_vert_pow -lt 11 ]
+while  [ $n_vert_pow -lt 8 ]
 do
-	n_vert=$(awk "BEGIN{print 3 ** $n_vert_pow}")
+	n_vert=$(awk "BEGIN{print 10 ** $n_vert_pow}")
 	./Randgraph/rg $graph_path $n_vert $graph_type 11
 	echo "generated graph with $n_vert vertices"
-	echo "n_vert=$n_vert">>$output_file
 	#iterate over num_cores
 	core_power="0"
-	while [ $core_power -lt 5 ]
+	while [ $core_power -lt 6 ]
 	do
 		n_core=$(awk "BEGIN{print 2 ** $core_power}")
-		echo "n_thread=$n_core">>$output_file
 		#check the limit. We don't have more than 24 cores
-		if [ "$n_core" -gt $CORE_CAP ]; then
-			core_power="24"
+		if [ $n_core -gt $CORE_CAP ]; then
+			n_core="24"
 		fi
 		#exec all the algos
 		for exec in $executables
 		do
-		#	echo "executing $exec"
-			"bin/$exec" "$n_core" "$graph_path" 1 1 >> "$output_file"
+			printf "%s," "$n_vert" >>"$output_file"_"$exec"
+			printf "%s," "$n_core"  >>"$output_file"_"$exec"
+			time_res=`bin/$exec $n_core $graph_path 1 1`
+			printf "%s\n"  $time_res >> "$output_file"_"$exec"
 		done
 		core_power=$[$core_power+1]
 	done
 	n_vert_pow=$[$n_vert_pow+1]
 done
+
 
